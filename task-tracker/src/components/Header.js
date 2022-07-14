633ac9c8d05026533768c0229190f4a09c1b02c2
Young-r-e-n <youngrenowola@gmail.com>
2022-07-14 19:36:03 +0300

    First commit

---
+++ task-tracker/src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+const Header = ({title}) => {
+
+  const onClick = () => {
+    console.log("clicked")
+  }
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='hello' onClick = {onClick}/>
+    </header>
+  )
+}
+//const headingStyle = { 
+ // color: "red", backgroundColor:"black"
+//}
+Header.defaultProps = { 
+ title: "Task Tracer", 
+}
+Header.propTypes ={ 
+  title: PropTypes.string.isRequired,
+}
+
+export default Header
