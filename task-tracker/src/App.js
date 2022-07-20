12472fe77424f2b34c4de6a969363db77fe1e3b4
Young-r-e-n <youngrenowola@gmail.com>
2022-07-20 12:29:13 +0300

    This is the latest commit

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,9 +1,37 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from 'react'
 
 function App() {
+
+  const[tasks,setTasks] = useState(
+   [
+    {
+      id: 1,
+      text: "Code in Python",
+      day: "Monday",
+      reminder: false
+    },
+    {
+      id: 2,
+      text: "Code in Jav",
+      day: "Tuesday",
+      reminder: false
+    },
+    {
+      id: 3,
+      text: "Code in Python",
+      day: "Monday",
+      reminder: false
+    }
+
+   ]
+    )
+
   return (
     <div className="container">
      <Header  />
+     <Tasks tasks = {tasks}/>
      </div>
   );
 }
